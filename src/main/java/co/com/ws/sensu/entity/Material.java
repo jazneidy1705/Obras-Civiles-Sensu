89b21ea6605778ea5bd912918b77995ace0b4e88
jazneidy <jazneidy1705@gmail.com>
2018-10-03 10:05:59 -0500

    se cambia nombre de la entidad producto a Material

--- src/main/java/co/com/ws/sensu/entity/Material.java
+++ src/main/java/co/com/ws/sensu/entity/Material.java
@@ -14,7 +14,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-@Table(name = "PRODUCTOS")
+@Table(name = "MATERIAL")
 public class Material implements Serializable {
 
 	@Id
